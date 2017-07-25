b257810c8d7470b367a1e944cc540b530451f8c5
TalusRocks <jensenrs@gmail.com>
2017-07-25 16:09:43 -0700

    Add files via upload

--- js/rebecca.js
+++ js/rebecca.js
@@ -13,18 +13,18 @@ $(".menu-burger").click(function(){
 
 /* To-Top button */
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topBtn").style.display = "block";
-    } else {
-        document.getElementById("topBtn").style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("topBtn").style.display = "block";
+//     } else {
+//         document.getElementById("topBtn").style.display = "none";
+//     }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+//     document.documentElement.scrollTop = 0; // For IE and Firefox
+// }
